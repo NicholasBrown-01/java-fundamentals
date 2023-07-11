3621a8f26d1c7fdf3e36ab54b8efa59dda103194
NicholasBrown-01 <nicholasbrown.us@gmail.com>
2023-07-10 19:11:01 -0500

    Lab01 Pluralize Method Complete

---
+++ basics/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please provide an animal (Ex: Dog, Cat etc):");
+        String userWord = String.valueOf(scanner.nextLine());
+
+        System.out.println("Please input number 0,1, or 2:");
+        int userNumber = Integer.valueOf(scanner.nextLine());
+
+        pluralize(userWord, userNumber);
+        
+    }
+
+    static void pluralize(String userWord, int userNumber) {
+    
+        if (userNumber > 1 || userNumber == 0) {
+            System.out.println("I own "+userNumber+" "+userWord+"s.");
+            
+        } else {
+            System.out.println("I own "+userNumber+" "+userWord+".");
+        }
+    
+    }
+}
