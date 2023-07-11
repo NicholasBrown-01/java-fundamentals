5aca81075a178ecc6205a8bf0dd4615e655afee5
NicholasBrown-01 <nicholasbrown.us@gmail.com>
2023-07-10 19:13:08 -0500

    Updated Readme

--- basics/Main.java
+++ basics/Main.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        //Pluralize Method
+
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Please provide an animal (Ex: Dog, Cat etc):");
